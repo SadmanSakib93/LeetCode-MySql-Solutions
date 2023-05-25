c9637692f6c405f8d999d9d03f91528d0f7e91e4
Sadman Sakib <sadman.93.sakib@gmail.com>
2023-05-24 23:02:21 -0400

    Add files via upload

---
+++ 1407. Top Travellers.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/top-travellers/
+SELECT u.name AS name, coalesce(SUM(r.distance), 0) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r ON u.id=r.user_id
+GROUP BY r.user_id
+ORDER BY travelled_distance DESC, name
+
+
